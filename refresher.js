2c9e1b9cf4e99894978efbcec105fe2570984166
oliviaB <obooth@brooksschool.org>
2025-02-27 14:31:07 -0500

    did some code

---
+++ refresher.js
@@ -0,0 +1,52 @@
+//console.log('Hello World.')
+
+let x = 'how are you?'
+//console.log(x)
+
+let greeting = 'Hello'
+let fName = 'Bgob'
+//console.log(greeting + ' ' + fName + '!')
+
+let age = 13
+//console.log('you are '+ age)
+
+// console.log('In ten years you\'ll be ' + (age + 10))
+
+// if (fName === 'Bob') {
+//     console.log('I knew it was you, ' + fName)
+// } else {
+//     console.log('Hey' + fName + ', you\'re not Bob!')
+// }
+
+// let iAmWearingABelt = false
+// let iAmWearingGlasses = false
+
+// if (iAmWearingABelt && iAmWearingGlasses) {
+//     console.log('Probably not Mr. Gorton')
+
+// }
+
+// if (iAmWearingABelt && !iAmWearingGlasses) {
+//     console.log('Probably is Mr. Gorton')
+
+// }
+// if (iAmWearingABelt || iAmWearingGlasses) {
+//     console.log('Probably just some dude')
+
+// }
+
+// for (let i = 0; i < 11; i++) {
+//     console.log(i);
+
+// }
+
+
+// for (let i = 0; i < 7; i++) {
+//     console.log('comp sci is awesome')
+// }
+
+function tellMyAge(){
+    console.log('You are ' + age + ' 13 years old.')
+}
+
+tellMyAge
