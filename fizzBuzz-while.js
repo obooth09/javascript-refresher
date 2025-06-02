abe1e6f915b0f2f2aeea8d0dd83c32ed55c092cb
oliviaB <obooth@brooksschool.org>
2025-06-02 12:00:53 -0400

    made code

--- fizzBuzz-while.js
+++ fizzBuzz-while.js
@@ -1,44 +1,24 @@
-let fizzes = 0
-let buzzes = 0
-let fizzBuzzes = 0
+let fizz = 0
+let buzz = 0
+let fizzBuzz = 0
 
-// while (fizzes < 1001) {
-//     for (let x = 1; x++;) {
-//         if (x % 3 == 0 && x % 5 == 0) {
-//             console.log(x + ' fizz buzz')
-//             fizzBuzzes = fizzBuzzes + 1
-//         } else if (x % 3 == 0) {
-//             console.log(x +' fizz')
-//             fizzes = fizzes + 1
-//         } else if (x % 5 == 0) {
-//             console.log(x + ' buzz')
-//             buzzes = buzzes + 1
-//         } else {
-//             console.log (x)
-//         }
-    
-    
-//     }
-    
-// }
-// console.log('fizzes')
 
-do {
-    for (let x = 1; x < 1001; x++) {
-                if (x % 3 == 0 && x % 5 == 0) {
-                    console.log(x + ' fizz buzz')
-                    fizzBuzzes = fizzBuzzes + 1
-                } else if (x % 3 == 0) {
-                    console.log(x +' fizz')
-                    fizzes = fizzes + 1
-                } else if (x % 5 == 0) {
-                    console.log(x + ' buzz')
-                    buzzes = buzzes + 1
-                } else {
-                    console.log (x)
-                }
+while (fizzes<1000) {
 
+
+    if (i % 15 == 0) {
+        console.log(i + " fizz buzz")
+        fizzBuzz = fizzBuzz + 1
+    } else if (i % 3 == 0) {
+        console.log(i + " fizz")
+        fizz = fizz + 1
+    } else if (i % 5 == 0) {
+        console.log(i + " buzz")
+        buzz = buzz + 1
+    } else {
+        console.log(i)
     }
-    
-}while (fizzes < 1001);
-console.log(fizzes)
+}
+
+
+console.log("There were " + fizz + " fizz's, " + buzz + " buzz's, and " + fizzBuzz + " fizzbuzz's out of " + maxNumber + ".")
--- real-javascript/myScript.js
+++ real-javascript/myScript.js
@@ -1,3 +1,15 @@
-console.log('Hello World')
-document.getElementById('mine').innerHTML = 'no'
-window.alert('I am an alert')
+
+
+function doStuff(){
+    let name = document.getElementById('name').value
+    let animal = document.getElementById('animal').value
+    let msg = "Hello " + name + ". Don't you wish you were a " + animal + "?"
+    
+    let myH = document.getElementById('myH').innerHTML = msg
+    myH.innerHTML = msg
+}
+
+function changeColor(){
+    console.log('working')
+    document.getElementById('name').style.backgroundColor = 'red'
+}
